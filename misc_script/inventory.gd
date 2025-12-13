e043fc5914bae7d6cc83c3851037800846adbce3
yannis300307 <52321382+yannis300307@users.noreply.github.com>
2025-12-13 13:30:16 +0100

    Limit camera, collisions and fix player movements

--- misc_script/inventory.gd
+++ misc_script/inventory.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	for i in $Items.get_child_count():
-		pass
+	for i in len(Global.inventory):
+		if i >= $Items.get_child_count():
+			break
+		var item: Sprite2D = $Items.get_child(i)
+		item.texture = Global.get_item_image(Global.inventory[i])
+		$Items.get_child(i).visible = true
+
 	
+	if len(Global.inventory) < $Items.get_child_count():
+		for i in range(len(Global.inventory), $Items.get_child_count()):
+			$Items.get_child(i).visible = false
--- objects/player.gd
+++ objects/player.gd
@@ -1,11 +1,9 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed: float = 35
 var acceleration: float = 0
 
 var direction = Vector2.ZERO
-var velocity: Vector2 = Vector2.ZERO # The player's movement vector.
-
 
 # Called when the node enters the scene tree for the first time.
 func _process(_delta: float) -> void:
@@ -23,22 +21,22 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	var acc_state: int = 0
 	if Input.is_action_pressed("ui_right"):
-		acceleration = min(acceleration + (0.142 * delta), 1)
+		acceleration = min(acceleration + (0.2 * delta), 1)
 		direction.x = 1;
 		#rotation_degrees = 90
 	elif Input.is_action_pressed("ui_left"):
-		acceleration = min(acceleration + (0.142 * delta), 1)
+		acceleration = min(acceleration + (0.2 * delta), 1)
 		direction.x = -1
 		#rotation_degrees = -90
 	else:
 		acc_state = 1
 		direction.x = 0
 	if Input.is_action_pressed("ui_down"):
-		acceleration = min(acceleration + (0.142 * delta), 1)
+		acceleration = min(acceleration + (0.2 * delta), 1)
 		direction.y = 1
 		#rotation_degrees = 180
 	elif Input.is_action_pressed("ui_up"):
-		acceleration = min(acceleration + (0.142 * delta), 1)
+		acceleration = min(acceleration + (0.2 * delta), 1)
 		direction.y = -1
 		#rotation_degrees = 0
 	else:
@@ -49,10 +47,8 @@ func _physics_process(delta: float) -> void:
 	if direction.x or direction.y:
 		$Normal.rotation = direction.angle()
 	var tmp: float = ease_out_expo(acceleration) * speed * delta
-	velocity.y = tmp * direction.y
-	velocity.x = tmp * direction.x
-	velocity = velocity.limit_length(tmp)
-	position += velocity * speed
+	var motion = tmp * direction.limit_length(tmp)
+	move_and_collide(motion * speed)
 
 func ease_out_expo(x: float) -> float:
 	return (1 - pow(2, -10 * x))
