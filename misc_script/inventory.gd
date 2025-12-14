2d1da9f6c65a21f9120ca05cbc7858e96cd44ea8
5BNeumann <flyalalu2@gmail.com>
2025-12-14 18:09:43 +0100

    f sound

--- misc_script/inventory.gd
+++ misc_script/inventory.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready() -> void:
 	visible = true
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	for i in len(Global.inventory):
 		if i >= $Elements/Items.get_child_count():
 			break
@@ -13,10 +13,10 @@ func _process(delta: float) -> void:
 		item.texture = Global.get_item_image(Global.inventory[i])
 		$Elements/Items.get_child(i).visible = true
 	
-	var player_screen_pos = %Player.get_global_transform_with_canvas()
+	#var player_screen_pos = %Player.get_global_transform_with_canvas()
 	#if player_screen_pos.xx < move_threshold:
 		#pass
-	print(player_screen_pos)
+	#print(player_screen_pos)
 
 	
 	if len(Global.inventory) < $Elements/Items.get_child_count():
--- objects/item_pool.gd
+++ objects/item_pool.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 var player_in_area = false
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body != %Player:
 		return
